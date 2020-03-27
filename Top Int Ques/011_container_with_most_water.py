f7008f43599a99461fbbb6a3278f5d4225fe3087
Rajarshi Basak <rajarshi.basak@colorado.edu>
2020-03-27 13:45:40 -0700

    Update 011_container_with_most_water.py

--- Top Int Ques/011_container_with_most_water.py
+++ Top Int Ques/011_container_with_most_water.py
@@ -7,8 +7,18 @@ class Solution(object):
         start = 0
         end = len(height) - 1
         largest = 0
+        prev_start = 0
+        prev_end = 0
         
         while start != end:
+            
+            if height[start] < prev_start:
+                start += 1
+                continue
+                
+            if height[end] < prev_end:
+                end -= 1
+                continue
             
             next_area = min(height[start], height[end])*(end - start)
             
@@ -16,8 +26,10 @@ class Solution(object):
                 largest = next_area
                 
             if height[start] < height[end]:
+                prev_start = height[start]
                 start += 1
             else:
+                prev_end = height[end]
                 end -= 1
                 
         return largest
