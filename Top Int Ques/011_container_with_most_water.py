c2bc9288db2c8a4e2d0472bed4fdc29e87e4cabd
Rajarshi Basak <rajarshi.basak@colorado.edu>
2020-03-27 13:30:43 -0700

    Create 011_container_with_most_water.py

---
+++ Top Int Ques/011_container_with_most_water.py
@@ -0,0 +1,23 @@
+class Solution(object):
+    def maxArea(self, height):
+        """
+        :type height: List[int]
+        :rtype: int
+        """
+        start = 0
+        end = len(height) - 1
+        largest = 0
+        
+        while start != end:
+            
+            next_area = min(height[start], height[end])*(end - start)
+            
+            if next_area > largest:
+                largest = next_area
+                
+            if height[start] < height[end]:
+                start += 1
+            else:
+                end -= 1
+                
+        return largest
