888a3175001fb7c25be6b69d2897d955c91b27dc
Rajarshi Basak <rajarshi.basak@colorado.edu>
2020-03-28 14:22:57 -0700

    Create 015_3_sum.py

---
+++ Top Int Ques/015_3_sum.py
@@ -0,0 +1,34 @@
+class Solution(object):
+    def threeSum(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[List[int]]
+        """
+        result = []
+        nums.sort()
+
+        for i in range(len(nums) - 2):
+            if i > 0 and nums[i] == nums[i-1]:
+                continue
+            left = i + 1
+            right = len(nums) - 1
+            
+            while left < right:
+                tot = nums[i] + nums[left] + nums[right]
+             
+                if tot < 0:
+                    left += 1
+                
+                elif tot > 0:
+                    right -= 1
+                    
+                else:
+                    result.append([nums[i], nums[left], nums[right]])
+                    while left < right and nums[left] == nums[left+1]:
+                        left += 1
+                    while left < right and nums[right] == nums[right-1]:
+                        right -= 1
+                    left += 1
+                    right -= 1
+        
+        return result
