35319f414b635c44353935da0f20dbba2de61520
Rajarshi Basak <rajarshi.basak@colorado.edu>
2020-04-21 15:07:19 -0700

    Update 008_string_to_integer_atoi.py

--- Top Int Ques/008_string_to_integer_atoi.py
+++ Top Int Ques/008_string_to_integer_atoi.py
@@ -4,40 +4,36 @@ class Solution(object):
         :type str: str
         :rtype: int
         """
-        
-        stripped = str.lstrip(" ")
-        if stripped == '':
+        remove_spaces = str.lstrip(" ")
+        if remove_spaces is None or len(remove_spaces) == 0:
             return 0
-        negative = False
-        remove_neg = None
-        if stripped[0] == '-':
-            negative = True
-            remove_neg = stripped[1:]
-        elif stripped[0] == '+':
-            remove_neg = stripped[1:]
-        if remove_neg is not None:
-            if remove_neg == '':
-                return 0
-            if remove_neg[0].isdigit() == False:
-                return 0
+        
+        isneg = False
+        remove_sign = ''
+        if remove_spaces[0] == '+':
+            remove_sign = remove_spaces[1:]
+        elif remove_spaces[0] == '-':
+            remove_sign = remove_spaces[1:]
+            isneg = True
+        elif remove_spaces[0].isdigit() == False:
+            return 0
         else:
-            remove_neg = stripped
-        remove_char = ''
-        for char in remove_neg:
+            remove_sign = remove_spaces
+            
+        result = 0
+        for char in remove_sign:
             if char.isdigit() == True:
-                remove_char = remove_char + char
+                result = result*10 + int(char)
             else:
                 break
-        result = 0
-        for char in remove_char:
-            result = 10*result + int(char)
-        if result > (2)**31-1:
-            if negative == True:
+
+        if isneg == True:
+            if result > 2**31 - 1:
                 return (-2)**31
             else:
-                return (2)**31 - 1
+                return (result - 2*result)
         else:
-            if negative == True:
-                return result - 2*result
+            if result > 2**31 - 1:
+                return 2**31 - 1
             else:
                 return result
