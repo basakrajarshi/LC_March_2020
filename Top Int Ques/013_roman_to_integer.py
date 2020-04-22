b8d3f3abd91b1d16e9a107b9fde93032ebf6a25c
Rajarshi Basak <rajarshi.basak@colorado.edu>
2020-04-22 13:22:22 -0700

    Update 013_roman_to_integer.py

--- Top Int Ques/013_roman_to_integer.py
+++ Top Int Ques/013_roman_to_integer.py
@@ -4,39 +4,35 @@ class Solution(object):
         :type s: str
         :rtype: int
         """
-        def get_integer_value(x):
-            if x == 'I':
-                return 1
-            if x == 'V':
-                return 5
-            if x == 'X':
-                return 10
-            if x == 'L':
-                return 50
-            if x == 'C':
-                return 100
-            if x == 'D':
-                return 500
-            if x == 'M':
-                return 1000
+        if s == None or len(s) == 0:
+            return 0
+        
+        d = {'I': 1,
+             'V': 5,
+             'X': 10,
+             'L': 50,
+             'C': 100,
+             'D': 500,
+             'M': 1000}
         
         result = 0
-        i = 0 
+        
+        i = 0
         while i < len(s):
-            s1 = get_integer_value(s[i])
             
-            if i+1 < len(s):
-                s2 = get_integer_value(s[i+1])
+            val1 = d[s[i]]
+            if i < len(s) - 1:
+                val2 = d[s[i+1]]
                 
-                if s1 >= s2:
-                    result = result + s1
+                if val1 >= val2:
+                    result = result + val1
                     i += 1
                 else:
-                    result = result + s2 - s1
+                    result = result + val2 - val1
                     i += 2
-            
+                    
             else:
-                result = result + s1
+                result = result + val1
                 i += 1
-                
-        return result
+            
+        return result    
