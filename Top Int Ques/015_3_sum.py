b2969c021bf6553d0d00c9fe91c03eceb6452676
Rajarshi Basak <rajarshi.basak@colorado.edu>
2020-04-22 20:30:12 -0700

    Update 015_3_sum.py

--- Top Int Ques/015_3_sum.py
+++ Top Int Ques/015_3_sum.py
@@ -5,30 +5,48 @@ class Solution(object):
         :rtype: List[List[int]]
         """
         result = []
+        # Sort the array in ascending order
         nums.sort()
-
+        
+        # Go through all numbers in the array except the last two
         for i in range(len(nums) - 2):
+            
+            # Move forward if the first index (i) has duplicate values
             if i > 0 and nums[i] == nums[i-1]:
                 continue
+            
+            # Create second index
             left = i + 1
+            
+            # Create third index
             right = len(nums) - 1
             
+            # While second index is less than third index
             while left < right:
-                tot = nums[i] + nums[left] + nums[right]
-             
-                if tot < 0:
+                
+                # Find the sum of the three numbers
+                total = nums[i] + nums[left] + nums[right]
+                
+                # If sum < 0, increase left index
+                if total < 0:
                     left += 1
                 
-                elif tot > 0:
+                # If sum > 0, decrease right index
+                elif total > 0:
                     right -= 1
-                    
+                
+                # If sum = 0, add the triplet to results
                 else:
                     result.append([nums[i], nums[left], nums[right]])
-                    while left < right and nums[left] == nums[left+1]:
+                    # Skip duplicates found through second index
+                    while left < right and nums[left] == nums[left + 1]:
                         left += 1
-                    while left < right and nums[right] == nums[right-1]:
+                    # Skip duplicates found through third index
+                    while left < right and nums[right] == nums[right - 1]:
                         right -= 1
+                    
+                    # Increase both left and right index since sum found
                     left += 1
                     right -= 1
-        
-        return result
+                    
+        return result                 
