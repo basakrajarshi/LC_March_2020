01f30858a9b5fd61e06f0ad7c4f80ea034bafa58
Rajarshi Basak <rajarshi.basak@colorado.edu>
2020-04-25 14:06:53 -0700

    Update 029_divide_two_integers.py

--- Top Int Ques/029_divide_two_integers.py
+++ Top Int Ques/029_divide_two_integers.py
@@ -5,38 +5,62 @@ class Solution(object):
         :type divisor: int
         :rtype: int
         """
-        isneg = False
+        
+        if dividend == (-2)**31 and dividend == 1:
+            return dividend
+        if dividend == (-2)**31 and divisor == -1:
+            return (2**31 - 1)
+        if dividend < (-2)**31 or dividend > 2**31 - 1:
+            if abs(divisor) == 1:
+                return 2**31 - 1
+    
+        if dividend > 0 and divisor > 0 and dividend < divisor:
+            return 0
+        
+        if divisor == dividend:
+            return 1
+        if divisor == dividend * (-1):
+            return -1
+        if divisor == 1:
+            return dividend
+        if divisor == -1:
+            if dividend < 0:
+                return abs(dividend)
+            elif dividend > 0:
+                return dividend - 2*dividend
+            else:
+                return 0
         if dividend == 0:
             return 0
-        elif divisor == dividend:
-            result = 1
-        elif divisor == 1:
-            result = dividend
-        elif divisor == -1:
-            if dividend < 0:
-                result =  abs(dividend)
-            else:
-                result = (dividend - 2*dividend)
-        else:
+        
+        isneg_dividend = False
+        isneg_divisor = False
+        
+        if dividend < 0:
+            isneg_dividend = True
+        if divisor < 0:
+            isneg_divisor = True
             
-            if divisor < 0 and dividend > 0:
-                isneg = True
-            elif divisor > 0 and dividend < 0:
-                isneg = True
-            divisor = abs(divisor)
-            dividend = abs(dividend)
-            result = 0
-            counter = 0
-            while counter < dividend:
-                counter += divisor
-                if counter <= dividend:
-                    result += 1
-            #print(result)
+        mod_dividend = abs(dividend)
+        mod_divisor = abs(divisor)
+        
+        count = 1
+        new_divisor = mod_divisor
+        while new_divisor <= mod_dividend:
+            new_divisor += mod_divisor
+            count += 1
             
-        if isneg == True:
-            result = result - 2*result
-
-        if result < (-2)**31 or result > 2**31 - 1:
+            if new_divisor + mod_divisor > mod_dividend:
+                break
+        
+        if count > 2**31 - 1:
             return 2**31 - 1
+        
+        if isneg_dividend == True and isneg_divisor == True:
+            return count
+        elif isneg_dividend == True and isneg_divisor == False:
+            return count - 2*count
+        elif isneg_dividend == False and isneg_divisor == True:
+            return count - 2*count
         else:
-            return result
+            return count
