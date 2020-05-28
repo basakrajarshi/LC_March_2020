ed467df9ee1def436a960e36084ec11d32a9ff04
Rajarshi Basak <rajarshi.basak@colorado.edu>
2020-05-27 19:23:16 -0700

    Create 066_plus_one.py

---
+++ Top Int Ques/066_plus_one.py
@@ -0,0 +1,23 @@
+class Solution(object):
+    def plusOne(self, digits):
+        """
+        :type digits: List[int]
+        :rtype: List[int]
+        """
+        n = len(digits)
+                
+        for i in range(n-1,-1,-1):
+            if i > 0:
+                if digits[i] < 9:
+                    digits[i] += 1
+                    return digits
+                else:
+                    digits[i] = 0
+                    continue
+            else:
+                if digits[i] < 9:
+                    digits[i] += 1
+                    return digits
+                else:
+                    digits[i] = 0
+                    return [1] + digits
